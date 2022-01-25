8ef23a8d4d4541e62f767c1b64777e5f40b9a904
Nguyễn Thị Linh <libh1004@gmail.com>
2022-01-25 15:15:49 +0700

    Add project files.

---
+++ BankClient/Controllers/BankController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankClient.Controllers
+{
+    public class BankController : Controller
+    {
+        BankService.BankServiceClient bankclient = new BankService.BankServiceClient();
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult GetInformation()
+        {
+            return View();
+        }
+        public ActionResult WithDraw()
+        {
+            return View();
+        }
+        public ActionResult Deposit()
+        {
+            return View();
+        }
+        public ActionResult TransactionHistory()
+        {
+            return View();
+        }
+    }
+}
